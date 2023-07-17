25c59e110fd9cf2fe716b26eea421c12f40d7031
sumitkohli <sumitkohli2004@gmail.com>
2023-07-17 16:03:58 +0530

    Initial commit

--- basicarray/question1.java
+++ basicarray/question1.java
@@ -3,6 +3,7 @@ package basicarray;
 public class question1 {
     public static void main(String[] args) {
         System.out.println("hello , my name is sumit kohli");
+        System.out.println("this is my first step - ");
         
     }
 }
