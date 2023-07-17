c68f33162662100554d73c65f9da4b2b4137b8d6
sumitkohli <sumitkohli2004@gmail.com>
2023-07-17 16:12:25 +0530

    this is my first step

--- basicarray/question1.java
+++ basicarray/question1.java
@@ -4,6 +4,7 @@ public class question1 {
     public static void main(String[] args) {
         System.out.println("hello , my name is sumit kohli");
         System.out.println("this is my first step - ");
+        System.out.println("this is my sccond step -  ");
         
     }
 }
